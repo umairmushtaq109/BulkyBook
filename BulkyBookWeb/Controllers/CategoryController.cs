8849a6a3402f4a7a0fd090434047d69e18ccefb2
umairmushtaq109 <umairmushtaqreal@gmail.com>
2022-06-03 16:49:16 +0500

    Category
    Added category to database.

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.tbl_categories;
+            return View(categories);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.tbl_categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
