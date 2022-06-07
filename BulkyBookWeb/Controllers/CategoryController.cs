c292c9f5ec3d4cae31c861140da21902246aa96b
umairmushtaq109 <umairmushtaqreal@gmail.com>
2022-06-07 12:09:14 +0500

    Category
    Edit, Delete Functionality Added

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -30,9 +30,85 @@ namespace BulkyBookWeb.Controllers
         [AutoValidateAntiforgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.tbl_categories.Add(obj);
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and Display Order cannot be same!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.tbl_categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.tbl_categories.Find(id);
+            //var categoryFromFirst = _db.tbl_categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromSingle = _db.tbl_categories.SingleOrDefault(c => c.Id == id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Name and Display Order cannot be same!");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.tbl_categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.tbl_categories.Find(id);
+            //var categoryFromFirst = _db.tbl_categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFromSingle = _db.tbl_categories.SingleOrDefault(c => c.Id == id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost, ActionName("Delete")]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.tbl_categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }    
+            _db.tbl_categories.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
+            
         }
     }
 }
